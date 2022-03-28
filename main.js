b8a9f4012ecad213f2f3efaf539346511202fd77
1 <a1@1s-MacBook-Pro.local>
2022-03-28 10:34:42 -0400

    rev1

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 /*******************
  * OUR HELPER CODE *
-*******************/
+ *******************/
 
 /*
  * Here we add the squares to the canvas dynamically.
  * You can mostly leave this section alone!
  * But if you want to change how wide the canvas is,
  * there are just two steps:
- * 
+ *
  * 1. Change the `gridWidth` value below.
  * 2. Change the `grid-template-rows` and
  * `grid-template-columns` to match.
@@ -18,9 +18,9 @@
 const gridWidth = 10;
 let count = 0;
 while (count <= gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
+  const canvas = document.querySelector(".canvas");
+  const div = document.createElement("div");
+  div.className = "square color-5";
   canvas.appendChild(div);
   count++;
 }
@@ -44,16 +44,71 @@ while (count <= gridWidth * gridWidth) {
 
 /***********
  * QUERIES *
-***********/
+ ***********/
+changeColor(color1);
+let colors = document.querySelectorAll(".palette-color color-1");
 
+let color1 = document.querySelector("#color-1");
+let color2 = document.querySelector("#color-2");
+let color3 = document.querySelector("#color-3");
+let color4 = document.querySelector("#color-4");
+let color5 = document.querySelector("#color-5");
+
+let brush = document.querySelector(".brush-selection");
+
+let palette = document.querySelector(".palette");
+
+let canvas = document.querySelector(".canvas");
+
+//applying the the action to the individual name/ item
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
 
+changeColor1(color1);
+changeColor2(color2);
+changeColor3(color3);
+changeColor4(color4);
+changeColor5(color5);
 
+function changeColor1(element) {
+  element.style.backgroundColor = "white";
+}
+function changeColor2(element) {
+  element.style.backgroundColor = "blue";
+}
+function changeColor3(element) {
+  element.style.backgroundColor = "green";
+}
+function changeColor4(element) {
+  element.style.backgroundColor = "pink";
+}
+function changeColor5(element) {
+  element.style.backgroundColor = "purple";
+}
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
-****************************/
+ ****************************/
+
+colors.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+
+color1.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+color2.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+color3.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+color4.addEventListener("click", (e) => {
+  console.log(e.target);
+});
+color5.addEventListener("click", (e) => {
+  console.log(e.target);
+});
 
 // Now add some functions to handle clicking one particular square
 // and clicking one particular palette color. You can leave them
@@ -61,13 +116,42 @@ while (count <= gridWidth * gridWidth) {
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
 
-
-
 /**************************
  * WIRING IT ALL TOGETHER *
-**************************/
+ **************************/
 
 // Now: wiring up our event listeners to our html node elements.
 // You'll need to add the appropriate event listener for each
 // square and for each palette color from the functions you
 // wrote above.
+
+//this is applying the event listener to the parent ordered list,
+// the desribed action is being performed on all the children
+// indiviually however the action is the same for all.
+// //bubbling
+// let list1 = document.querySelector("ol");
+
+// //applyinth the action to the individual name/ item
+// list1.addEventListener("click", function (event) {
+//   event.target.style.textDecoration = "line-through";
+// });
+
+// let list2 = document.querySelector("ul");
+
+// list2.addEventListener("click", function (event) {
+//   event.target.style.opacity = 0;
+// });
+
+// let dino1 = document.querySelector("row");
+
+// dino1.addEventListener("click", function (event) {
+//   event.target.style.opacity = 0;
+// });
+
+// // class is .
+// // # id
+// //id="row"
+
+// // evvent.target
+
+// //
